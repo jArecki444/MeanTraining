ef09359c0dd58026524613b14b3f3c7d27f45ef2
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-02-12 14:14:19 +0100

    Create first blank component & remove placeholder

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,12 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PostsCreateComponent } from './posts/posts-create/posts-create.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PostsCreateComponent
   ],
   imports: [
     BrowserModule,
