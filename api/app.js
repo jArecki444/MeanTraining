91273d4d2fb454cf90b07a9408d48cc70ca98489
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-02-18 14:27:16 +0100

    First api json response

--- api/app.js
+++ api/app.js
@@ -2,13 +2,24 @@ const express = require("express");
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("First middleware");
-  next();
-});
+app.use("/api/posts", (req, res, next) => {
+  const posts = [
+    {
+      id: "1abc",
+      title: "First server-side post",
+      content: "This is api response",
+    },
+    {
+      id: "2eee",
+      title: "Second server-side post",
+      content: "This is second api response",
+    },
+  ];
 
-app.use((req, res, next) => {
-  res.send("Hello from express!");
+  res.status(200).json({
+    message: "Posts fetched successfully!",
+    posts: posts,
+  });
 });
 
 module.exports = app;
