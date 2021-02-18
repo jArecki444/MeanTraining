a8792d0c5dc8344470bf80d21adb8f694413582d
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-02-18 16:31:31 +0100

    Create model with mongoose lib

--- api/app.js
+++ api/app.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const Post = require("./models/post");
+
 const app = express();
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
@@ -19,7 +21,10 @@ app.use((req, res, next) => {
 });
 
 app.post("/api/posts", (req, res, next) => {
-  const post = req.body;
+  const post = new Post({
+    title: req.body.title,
+    content: req.body.content,
+  });
   console.log(post);
   res.status(201).json({
     message: "Post added successfully",
@@ -45,5 +50,4 @@ app.get("/api/posts", (req, res, next) => {
     posts: posts,
   });
 });
-
 module.exports = app;
