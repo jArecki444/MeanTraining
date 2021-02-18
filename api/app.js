a17a00ee4b039ccffbacecaa2c3686e63ce6aa4d
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-02-18 16:48:59 +0100

    Connect and save data to database

--- api/app.js
+++ api/app.js
@@ -1,9 +1,21 @@
 const express = require("express");
 const bodyParser = require("body-parser");
-
+const mongoose = require("mongoose");
 const Post = require("./models/post");
 
 const app = express();
+
+mongoose
+  .connect(
+    "mongodb+srv://jArecki:zUqmRDKZQoHfqzdL@cluster0.91s2x.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
+  )
+  .then(() => {
+    console.log("Connected to database!");
+  })
+  .catch(() => {
+    console.log("Connection failed!");
+  });
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
@@ -25,7 +37,7 @@ app.post("/api/posts", (req, res, next) => {
     title: req.body.title,
     content: req.body.content,
   });
-  console.log(post);
+  post.save();
   res.status(201).json({
     message: "Post added successfully",
   });
