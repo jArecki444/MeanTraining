a1967b3050fafd17d7ba29947e2f5ec2e7a189a7
Arkadiusz Cichurski <arkadiusz.cichurski@itcraft.pl>
2021-02-21 22:56:14 +0100

    Re-Organizing Backend Routes

--- api/app.js
+++ api/app.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const mongoose = require("mongoose");
-const Post = require("./models/post");
+const postsRoutes = require("./routes/posts");
 
 const app = express();
 
@@ -32,57 +32,6 @@ app.use((req, res, next) => {
   next();
 });
 
-app.post("/api/posts", (req, res, next) => {
-  const post = new Post({
-    title: req.body.title,
-    content: req.body.content,
-  });
-  post.save();
-  res.status(201).json({
-    message: "Post added successfully",
-  });
-});
+app.use("/api/posts", postsRoutes);
 
-app.put("/api/posts/:id", (req, res, next) => {
-  const post = new Post({
-    _id: req.params.id,
-    title: req.body.title,
-    content: req.body.content,
-  });
-  Post.updateOne({ _id: req.params.id }, post)
-    .then((response) => {
-      res.status(201).json({
-        message: "Post updated successfully",
-      });
-    })
-    .catch((err) => console.log("Catched error!", err));
-});
-
-app.get("/api/posts", (req, res, next) => {
-  Post.find().then((documents) => {
-    res.status(200).json({
-      message: "Posts fetched successfully!",
-      posts: documents,
-    });
-  });
-});
-
-app.get("/api/posts/:id", (req, res, next) => {
-  Post.findById(req.params.id).then((post) => {
-    if (post) {
-      res.status(200).json(post);
-    } else {
-      res.status(404).json({ message: "Post not found!" });
-    }
-  });
-});
-
-app.delete("/api/posts/:id", (req, res, next) => {
-  console.log(req.params);
-  Post.deleteOne({ _id: req.params.id })
-    .then(() => {
-      res.status(200).json({ message: "Post deleted!" });
-    })
-    .catch((err) => console.log("Catched error!", err));
-});
 module.exports = app;
